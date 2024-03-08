3c5bddf0b45014328d56164dff28e64565602392
AbhayKamath03 <92569661+AbhayKamath03@users.noreply.github.com>
2024-03-08 15:28:19 +0530

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, I am Abhay Kamath\n";
+  cout << "My SRN is PES2UG21CS012\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
